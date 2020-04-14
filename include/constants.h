50ab1493df5aa9c7284d61ce8f10d975d744569b
Daniel Escobar <escobar.saltaren@gmail.com>
2020-04-14 17:44:34 -0500

    Basic Functionality
    Constants.h added
    Connect.h added
    
    Basic MQTT functionality added at main. Read from A0 and print.

---
+++ include/constants.h
@@ -0,0 +1,13 @@
+#define SSID "TIGO-A03D"
+#define SSID_PASSWORD "2NB144201853"
+
+#define MQTT_ID "medDevice"
+#define MQTT_USER "medDevicePrototype"
+#define MQTT_PASSWORD "PASSWORD"
+
+#define BROKER "192.168.1.7"
+
+#define REPORT_FREQ 2000
+#define MAX_ALARM_FREQ 10000
+
+#define INPUT_BTN 5
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
-#include <arduino.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <MQTT.h>
 
-void setup(){
+#include <constants.h>
+#include <connect.h>
 
+WiFiClient net;
+MQTTClient client;
+
+long reportTimer = millis();
+long alarmTimer = millis();
+
+void setup() {
+  
+  pinMode(INPUT_BTN, INPUT);
+
+  Serial.begin(115200);
+  WiFi.begin(SSID, SSID_PASSWORD);
+
+  client.begin(BROKER, net);
+
+  connect(Serial, net, client);
 }
 
-void loop(){
+void loop() {
+  client.loop();
 
+  if (!client.connected()) {
+    connect(Serial, net, client);
+  }
+
+  if(millis()-reportTimer >= REPORT_FREQ){
+    reportTimer=millis();
+    client.publish(String(MQTT_ID)+"/read", String(analogRead(A0)));
+  }
+
+  if(( millis()-alarmTimer >= MAX_ALARM_FREQ)){    
+    alarmTimer = millis();
+    client.publish(String(MQTT_ID)+"/alarm", "Warning!");
+    delay(500);
+  }
 }
